2265ff925f5a50cc16b4eae247851c6988d585c9
urnice1111 <emilianogarramxd@gmail.com>
2026-03-17 20:29:45 -0600

    Test commit for components folder

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,15 @@
+import logo from '../assets/logo_itec.png'
+import './Landing.css'
+
+
 function Landing(){
 
     return(
-        <div>
-            <h1>iTec</h1>
+        <div className='landingContainer'>
+            <img src={logo} alt="iTec Logo" className='logo' />
+            <h1 className='title'>iTec</h1>
         </div>
-    )
+    )   
 
 
 }
