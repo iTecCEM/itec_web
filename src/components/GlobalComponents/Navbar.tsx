7885f942a489e6ae1d04bf3fb401e42659a902fa
urnice1111 <emilianogarramxd@gmail.com>
2026-03-18 20:39:46 -0600

    changed phto error

---
+++ src/components/GlobalComponents/Navbar.tsx
@@ -0,0 +1,22 @@
+import logo from '../assets/logo_itec.png'
+import './GlobalComponents.css'
+
+const Navbar = () => {
+    return (
+        <header className = "navbar-container">
+                <nav className = 'navbar-content'>
+                    <a href="#" className="navbar-logo">
+                        <img src = {logo} alt= "iTec logo"/>
+                    </a>
+                    <ul className = "navbar-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">About us</a></li>
+                    </ul>
+                </nav>
+        </header>
+    );
+};
+
+export default Navbar
