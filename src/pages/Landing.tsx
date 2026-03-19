1aec5f13359b32148531bf4080bdb75053ba5af9
urnice1111 <emilianogarramxd@gmail.com>
2026-03-18 20:52:55 -0600

    A

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,11 +2,13 @@ import './Landing.css'
 import FirstLanding from '../components/LandingPageComponents/FirstLanding'
 import MainEvent from '../components/LandingPageComponents/MainEvent'
 import EventsGrid from '../components/LandingPageComponents/EventsGrid'
+import NavBar from '../components/GlobalComponents/Navbar.tsx'
 
 function Landing(){
 
     return(
         <div className='landingContainer'>
+            <NavBar/>
             <FirstLanding />
             <hr className="Divider" />
             <MainEvent />
